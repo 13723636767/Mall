bdfe5d4f48a4ff994f2d83e868791cf79c5f1f21
13723636767 <1783363391@qq.com>
2021-01-03 15:10:51 +0800

    文件更新

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FastClick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
 Vue.use(VueLazyLoad, {
-  preLoad: 1,
-  loading: require('assets/img/common/placeholder.png')
+  preLoad: 1
 })
+
+FastClick.attach(document.body)
 
 new Vue({
   render: h => h(App),
